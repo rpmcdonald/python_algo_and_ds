06e194eefb484dffda030559483a702220bfa670
Ross McDonald <rosspmcd@gmail.com>
2022-12-09 23:30:07 +0000

    data structures

---
+++ data_structures.py
@@ -0,0 +1,67 @@
+from collections import deque
+import heapq
+
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        # return len(self.items) == 0
+        return not self.items
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+    def __str__(self):
+        return str(self.items)
+
+
+class Queue:
+    def __init__(self):
+        self.items = deque()
+
+    def is_empty(self):
+        return not self.items
+        # return len(self.items) == 0
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        return self.items.popleft()
+
+    def size(self):
+        return len(self.items)
+
+    def peek(self):
+        return self.items[0]
+
+    def __str__(self):
+        return str(self.items)
+
+
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+
+    def is_empty(self):
+        return not self.elements
+
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+
+    def __str__(self):
+        return str(self.elements)
